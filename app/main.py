58bf9eae995e3eaddc070ff2642830326ad6ff67
Oleksandr Shkliar <shklyara29@gmail.com>
2025-08-10 14:16:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    if command == "":
+        return
+    command_list = command.split()
+    if len(command_list) == 3 and command_list[0] != "cp":
+        return
+    if len(command_list) == 3 and command_list[1] == command_list[2]:
+        return
+    if not os.path.exists(command_list[1]):
+        return
+    with (open(command_list[1], "r") as file_in,
+          open(command_list[2], "w") as file_out):
+        read_content = file_in.read()
+        file_out.write(read_content)
